4386c9c03ea503081e8f4f5abba9be89d782b7ef
David Kunzmann <davidkunzmann@gmail.com>
2016-05-20 23:35:05 +0200

    Slick and MySql

--- server/app/database/Job.scala
+++ server/app/database/Job.scala
@@ -2,27 +2,35 @@ package database
 
 import scala.concurrent.Future
 
-import javax.inject.Inject
+import play.api.Play
+import javax.inject._
 import models.Job
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.db.slick.HasDatabaseConfigProvider
+import slick.driver.MySQLDriver.api._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import slick.driver.JdbcProfile
+import slick.lifted.Tag
 
-class JobDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
-  import driver.api._
-
-  private val Jobs = TableQuery[JobsTable]
-
-  def all(): Future[Seq[Job]] = db.run(Jobs.result)
-  def insert(job: Job): Future[Unit] = db.run(Jobs += job).map { _ => () }
-
-
-  private class JobsTable(tag: Tag) extends Table[Job](tag, "JOB") {
+trait JobComponent {
+  class JobsTable(tag: Tag) extends Table[Job](tag, "JOB") {
 
     def name = column[String]("NAME", O.PrimaryKey)
     def color = column[String]("COLOR")
 
     def * = (name, color) <> (Job.tupled, Job.unapply _)
   }
+
+}
+
+
+object JobDAO extends JobComponent {
+
+  // import dbConfig.driver.api._
+  protected val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+  private val Jobs = TableQuery[JobsTable]
+
+  def all(): Future[Seq[Job]] = dbConfig.db.run(Jobs.result)
+  def insert(job: Job): Future[Unit] = dbConfig.db.run(Jobs += job).map { _ => () }
+
 }
