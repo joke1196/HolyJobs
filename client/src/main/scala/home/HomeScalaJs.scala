245d18521a8cf16407e6dd6d029cbaee16342ff9
edri <mig.santamar@gmail.com>
2016-06-01 09:22:26 +0200

    Update GUI.

--- client/src/main/scala/home/HomeScalaJs.scala
+++ client/src/main/scala/home/HomeScalaJs.scala
@@ -11,6 +11,9 @@ object HomeScalaJs extends js.JSApp {
     // the main page.
     @JSExport
     def searchForJobs(): Unit = {
+        //val searchWhatField = dom.document.getElementById("search-what")
+        //val a = jQuery("#search-what").val()
+        //println(a)
         //val tmp = jQuery("#home-page-bottom").position().top;
         /*val tmp = dom.document.getElementById("home-page-bottom").getBoundingClientRect().top
         println(tmp)*/
